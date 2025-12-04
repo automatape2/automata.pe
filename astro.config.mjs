2247171a9f5960295d1ecc03f4c32e6628c035b4
Jose Anicama <j.j.anicamatanta@gmail.com>
2025-12-04 14:26:15 -0500

    feat: Configure Astro to always inline stylesheets in the build process.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    inlineStylesheets: 'always'
+  },
   i18n: {
     defaultLocale: 'en',
     locales: ['es', 'en'],
